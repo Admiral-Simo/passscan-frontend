52f02fe9a0bea54227bcc353c86a93880c7b5ee7
Admiral-Simo <admiralsimo16@gmail.com>
2024-06-13 01:14:18 +0100

    enhance the feature of drag and drop

--- src/app/drag-drop-passport/drag-drop-passport.component.ts
+++ src/app/drag-drop-passport/drag-drop-passport.component.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-drag-drop-passport',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './drag-drop-passport.component.html',
   styleUrls: ['./drag-drop-passport.component.css'],
 })
 export class DragDropPassportComponent {
   isDragging = false;
+  imageUrl: string | null = null;
 
   @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
 
@@ -44,7 +46,10 @@ export class DragDropPassportComponent {
   }
 
   handleFile(file: File): void {
-    // Handle the file (e.g., upload it, display a preview, etc.)
-    console.log('File selected:', file);
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.imageUrl = e.target.result;
+    };
+    reader.readAsDataURL(file);
   }
 }
