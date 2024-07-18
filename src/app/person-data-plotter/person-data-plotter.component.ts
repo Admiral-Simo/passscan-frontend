e03b4e4b94f9d78f5a78fd1e249698e3e0c03dfa
Admiral-Simo <admiralsimo16@gmail.com>
2024-07-18 12:41:24 +0100

    feat: copy json and download json to clipboard

--- src/app/person-data-plotter/person-data-plotter.component.ts
+++ src/app/person-data-plotter/person-data-plotter.component.ts
@@ -16,4 +16,32 @@ export class PersonDataPlotterComponent {
   personEntries(): [string, string][] {
     return Object.entries(this.data || {});
   }
+
+  jsonData(): string {
+    return JSON.stringify(this.data, null, 2);
+  }
+
+  downloadJSON(): void {
+    const text = JSON.stringify(this.data, null, 2);
+    const blob = new Blob([text], { type: 'application/json' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'data.json';
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+
+  copyJSON(): void {
+    const text = JSON.stringify(this.data, null, 2);
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        console.log('JSON copied to clipboard');
+      })
+      .catch((err) => {
+        console.error('Failed to copy JSON: ', err);
+      });
+    alert('copied json');
+  }
 }
