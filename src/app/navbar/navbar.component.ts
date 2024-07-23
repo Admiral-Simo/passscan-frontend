deadca22537f0bea8fbc28fe093fed9ba1e33263
Admiral-Simo <admiralsimo16@gmail.com>
2024-07-23 14:07:29 +0100

    feat: light and dark mode

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -12,6 +12,7 @@ export class NavbarComponent implements OnInit {
   @Input() firstPartTitle!: string;
   @Input() secondPartTitle!: string;
   @Output() urlChanged = new EventEmitter<string>();
+  isDarkMode: boolean = false;
 
   urls = [
     { lang: 'golang', endpoint: 'http://31.220.89.29:8091' },
@@ -22,10 +23,26 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
     this.urlChanged.emit(this.used_url);
+    this.isDarkMode = localStorage.getItem('darkMode') === 'true';
+    this.applyDarkMode();
   }
 
   setUsedUrl(url: string) {
     this.used_url = url;
     this.urlChanged.emit(this.used_url);
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('darkMode', this.isDarkMode.toString());
+    this.applyDarkMode();
+  }
+
+  applyDarkMode() {
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
   }
 }
