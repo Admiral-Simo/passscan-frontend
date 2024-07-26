867f0123e19b343b54f0c4175ed09451d8abdd8c
Admiral-Simo <admiralsimo16@gmail.com>
2024-07-26 12:41:09 +0100

    feat: get upload history

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CommonModule } from '@angular/common';
@@ -6,6 +6,7 @@ import { DragDropPassportComponent } from './drag-drop-passport/drag-drop-passpo
 import { Data } from '../types/PersonData';
 import { PersonDataPlotterComponent } from './person-data-plotter/person-data-plotter.component';
 import { FooterComponent } from './footer/footer.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -21,12 +22,39 @@ import { FooterComponent } from './footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   firstPartTitle = 'passport';
   secondPartTitle = 'scanner';
 
+  // this should get the data from this url and put it into the uploadHistoryData
+  uploadHistoryUrl = 'http://31.220.89.29:8091/get-upload-history';
+  uploadHistoryData: IUploadHistory | null = null;
+
   baseUrl = '';
   uploadUrl = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchUploadHistory();
+    console.log(this.uploadHistoryData);
+  }
+
+  stringified(): string {
+    return JSON.stringify(this.uploadHistoryData);
+  }
+
+  fetchUploadHistory() {
+    this.http.get<IUploadHistory>(this.uploadHistoryUrl).subscribe(
+      (data) => {
+        this.uploadHistoryData = data;
+        console.log('Upload history data:', this.uploadHistoryData);
+      },
+      (error) => {
+        console.error('Error fetching upload history data:', error);
+      },
+    );
+  }
 
   onChangeUrl(url: string) {
     this.baseUrl = url;
@@ -50,4 +78,12 @@ export class AppComponent {
   setDataToNull() {
     this.data = null;
   }
+
+  getKeys(obj: IUploadHistory): string[] {
+    return Object.keys(obj).reverse();
+  }
 }
+
+type IUploadHistory = {
+  [key: string]: string[];
+};
